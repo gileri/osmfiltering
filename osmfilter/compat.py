68c6c1225dba23f06525060f4b56d616da0b0523
Éric Gillet <eric.gillet+gh@linuxw.info>
2016-08-13 22:30:51 +0200

    Log to debug whether lxml/cElementTree is used

--- osmfilter/compat.py
+++ osmfilter/compat.py
@@ -1,9 +1,13 @@
+import logging
+
 try:
     from lxml import etree
+    logging.debug("Using lxml")
 except ImportError:
     try:
         # Python 2.5
         import xml.etree.cElementTree as etree
+        logging.debug("Using cElementTree")
     except ImportError:
         try:
             # Python 2.5
@@ -12,6 +16,7 @@ except ImportError:
             try:
                 # normal cElementTree install
                 import cElementTree as etree
+                logging.debug("Using cElementTree")
             except ImportError:
                 # normal ElementTree install
                 import elementtree.ElementTree as etree
